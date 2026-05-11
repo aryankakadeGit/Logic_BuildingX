897f6d01cbdf76d52c2dd105035a596a0fa727e3
AryanKakade <153463913+aryankakadeGit@users.noreply.github.com>
2026-05-11 23:49:50 +0530

    Initial Upload

---
+++ Data_Structures/Scenario_Based/program823.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class program823 {
+    public static void main(String[] args) {
+        Scanner sobj = new Scanner(System.in);
+        int iNo = 0;
+        System.out.println("Enter the number : ");
+        iNo = sobj.nextInt();
+        int idigit = 0,iCount = 0,iTemp = 0,ipow = 0,iSum  = 0,iCountTemp = 0;
+        ipow = 1;
+        iTemp = iNo;
+        // no of digits
+        while (iNo != 0) {
+            idigit = iNo % 10;
+            iCount++;
+            iNo = iNo / 10;
+        }
+        iNo = iTemp;
+        iCountTemp = iCount;
+        // y = iCount;
+        while (iNo != 0) {
+            idigit = iNo % 10;
+            ipow = 1;
+            // count power
+            while (iCount != 0) {
+                ipow = ipow * idigit;
+                iCount--;
+            }
+            iCount = iCountTemp;
+            iSum = iSum+ipow;
+            iNo = iNo / 10;
+        }
+        if(iSum == iTemp)
+        {
+            System.out.println(iTemp+" is an armstrong number ");
+        }
+        else
+        {
+            System.out.println(iTemp+" is not an armstrong number ");
+        }
+        sobj.close();
+    }
+}
