25cdb70f6196f6492c4368e11733fd372fe0dac7
AryanKakade <153463913+aryankakadeGit@users.noreply.github.com>
2026-07-05 14:55:20 +0530

    Initial Upload

---
+++ Python/program951.py
@@ -0,0 +1,16 @@
+def DisplayDigits(No):
+    iSum = 0
+    iDigit = 0
+    while(No!=0):
+        iDigit = No%10
+        #iSum = iSum +iDigit
+        print(iDigit)
+        No=No//10 
+        
+def main():
+    No = 0
+    print("Enter Number : ")
+    No = int(input())
+    DisplayDigits(No)
+    
+main()
